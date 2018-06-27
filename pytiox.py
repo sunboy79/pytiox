02b3600fa395f5c7f59bcccec9e98886e68a152f
Unknown <Steve.j.Hughes@gmail.com>
2018-06-27 18:26:52 +0100

    Add some simple initial functionality
    Can add states to file object and print out some info. Start of mesh object.

--- pytiox.py
+++ pytiox.py
@@ -5,7 +5,7 @@ import h5py
 class TIO_File:
     def __init__(self, filename):
         self.filename = filename
-        self.Open()
+        self.states = {}
     
     def Open(self):
         self.f = h5py.File(self.filename,'w')
@@ -15,11 +15,17 @@ class TIO_File:
 
     def Get_Info(self):
         pass
+    
+    def Print(self):
+        print("File {0}".format(self.filename))
+
+    def Add_State(self, statename):
+        self.states[statename] = TIO_State(statename)
 
 
 class TIO_State:
-
-    def __init__(self): 
+    def __init__(self, name):
+        self.name = name 
         pass
     
     def Open(self):
@@ -30,7 +36,16 @@ class TIO_State:
 
     def Get_Info(self):
         pass
+    
+    def Print(self):
+        print("State {0}".format(self.name))
 
-
+class TIO_Mesh:
+    def __init__(self, name):
+        pass
 
 tiofile = TIO_File("test.h5")
+tiofile.Add_State("state001")
+
+tiofile.Print()
+tiofile.states["state001"].Print()
